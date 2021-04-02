1343e02d734a3a03d0da879b07d2ba9d390be483
letbrasil <le.brasileiro@gmail.com>
2021-04-02 18:45:40 -0300

    add (unfinished) POST and PUT functions to OrdersController.js

--- server/controller/OrdersController.js
+++ server/controller/OrdersController.js
@@ -39,12 +39,32 @@ class OrdersController {
     return res.status(200).json(order);
   }
 
-  // static async createOrder(req, res) {
-  //   const newOrder = await req.body;
+  static async createOrder(req, res) {
+    const newOrder = await req.body;
+    // por enquanto, esta função cria somente o pedido com id, user_id, table, client_name e status(default)
+    // criar array de products para incluir no pedido
+    const order = await database.Orders.create(newOrder);
+    return res.status(201).json(order);
+  }
 
-  //   const order = await database.Orders.create(newOrder);
-  //   return res.status(201).json(order);
-  // }
+  static async updateOrder(req, res) {
+    const { orderId } = req.params;
+    const { table, client_name, status } = req.body;
+    // como editar os itens do pedido ??
+    // por enquanto, só é possível alterar table, client_name e status
+    await database.Orders.update({
+      table,
+      client_name,
+      status,
+    },
+    {
+      where: {
+        id: Number(orderId),
+      },
+    });
+    const updatedOrder = await database.Orders.findByPk(orderId);
+    return res.status(200).json(updatedOrder);
+  }
 
   static async deleteOrder(req, res) {
     const { orderId } = req.params;
--- server/routes/ordersRouter.js
+++ server/routes/ordersRouter.js
@@ -6,8 +6,8 @@ const router = Router();
 // aqui vai as requisições
 router.get('/', OrdersController.getAllOrders);
 router.get('/:orderId', OrdersController.getOrderById);
-// router.post('/', OrdersController.createOrder);
-// router.put('/:orderId', OrdersController.updateOrder);
+router.post('/', OrdersController.createOrder);
+router.put('/:orderId', OrdersController.updateOrder);
 router.delete('/:orderId', OrdersController.deleteOrder);
 
 module.exports = router;
