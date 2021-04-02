8384f7efebb71feaa5f9b551929d719576a084a5
letbrasil <le.brasileiro@gmail.com>
2021-04-02 05:59:04 -0300

    rename const PORT to port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./server/routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/', routes);
@@ -11,6 +11,6 @@ app.get('*', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
