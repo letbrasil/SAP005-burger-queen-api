96225ea48fd5f4e20d221475eb4b40b6e967b7f8
letbrasil <le.brasileiro@gmail.com>
2021-04-02 01:45:07 -0300

    fix products endpoint bug

--- server/db/models/productsorders.js
+++ server/db/models/productsorders.js
@@ -1,6 +1,5 @@
 'use strict';
 const { Model } = require('sequelize');
-const ProductsController = require('../../controller/ProductsController');
 
 module.exports = (sequelize, DataTypes) => {
   class ProductsOrders extends Model {
